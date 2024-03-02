c346e235c3b15a8091c79fbb8ebc85ae951f3d83
ZhengZh0u <ZhengZh0u>
2024-03-02 17:04:36 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import House from './components/House';
+import Room from './components/rooms';
+import React from 'react';
+import PrettyCounter from './components/PrettyCounter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <House>
+              <Room name="Livingroom">
+              </Room>
+              <Room name="Kitchen">
+              </Room>
+            </House>
         <a
           className="App-link"
           href="https://reactjs.org"
